b91ee3e40d4597165de23b28f7a2307c48e39bce
kiarashkia138 <kiarashkia138@gmail.com>
2023-01-05 21:41:56 +0330

    can make one dir but not more than one

--- projet.c
+++ projet.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <dir.h>
+#include <process.h>
 
 
 char string_inpu[256] ;
 char string_path[256] = "D:\\C\\project\\project-kiarashkia138" ;
 char* firstofpath ;
-int end = 1;
+int end = 1,count_in = 0;
 FILE *fp;
 FILE *undo ;
 
@@ -19,27 +23,26 @@ char* concat(const char *s1, const char *s2)
 }
 
 
-void create_fuc() // start from 16 
+void create_fuc() // start from 16     // will create only one directory need work for second and more directories !!!!!!!!
 {
     firstofpath = &string_path[0] ;
+    char* dir = &string_path[0] ;
     int i = 16 ;
     if(string_inpu[i] == '"') // be careful
     {
-        i++;
+        i += 2;
         int siz = 0;
         while(string_inpu[i] != '"')
         {
             char temp[100] ;
             int j = 0;
-            while(string_inpu[i] != '/' )
+            while(string_inpu[i] != '/' && string_inpu[i] != '"' )
             {
                 temp[j] = string_inpu[i];
                 j++;
                 i++;
                 siz++ ;
             }
-            // add string between 17 & i-1 into "D:\C\project\project-kiarashkia138\"
-            //  add with backslashe !! 
             char fin[siz] ;
             for (int k = 0; k < siz; k++)
             {
@@ -48,18 +51,35 @@ void create_fuc() // start from 16
             siz = 0 ;
             firstofpath = concat(firstofpath, "\\");
             firstofpath = concat(firstofpath, fin);
-            // printf("%s\n", firstofpath);
-            i++ ;
+
+            int l = i ;
+            int count_backslashe = 0 ;
+            while(string_inpu[l] != '"')
+            {
+                if(string_inpu[l] == '/')   
+                    count_backslashe++;
+                l++;
+            }
+            if(count_backslashe > 0)
+            {
+                dir = concat(dir,"\\");
+                dir = concat(dir,fin);
+            }
+            if(string_inpu[i] != '"') i++ ;
         }
         // need ro check a direcrory exits or not and create it 
+        // char* dirname = "tutorialspoint";
+        // clrscr();
+        // printf("%s\n",dir);
+        CreateDirectory (dir, NULL); // need to check
         fp = fopen(firstofpath,"w");  // will creat a file 
         fclose(fp);
     }
     else{
-
+        // (in this version i am changing to 17)
+        i++;
         while(string_inpu[i] != '\n' && string_inpu[i] != ' ' && string_inpu[i] != '\0')
         {
-            // char* temp = (char*)malloc(100*sizeof(char));
             char temp[100] ;
             int siz = 0;
             int j = 0;
@@ -80,10 +100,25 @@ void create_fuc() // start from 16
             siz = 0 ;
             firstofpath = concat(firstofpath, "\\");
             firstofpath = concat(firstofpath, fin);
-            // printf("%s\n", firstofpath);
+            int l = i ;
+            int count_backslashe = 0 ;
+            while(string_inpu[l] != '\n' && string_inpu[l] != ' ' && string_inpu[l] != '\0')
+            {
+                if(string_inpu[l] == '/')   
+                    count_backslashe++;
+                l++;
+            }
+            if(count_backslashe > 0)
+            {
+                dir = concat(dir,"\\");
+                dir = concat(dir,fin);
+            }
             i++ ;
         }
-        // need ro check a direcrory exits or not and create it 
+        // need ro check a direcrory exits or not and create it
+        // printf("%s\n",dir);
+        // printf("%s\n",firstofpath);
+        CreateDirectory (dir, NULL); 
         fp = fopen(firstofpath,"w");  // will creat a file 
         fclose(fp);
     }
