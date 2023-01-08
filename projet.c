1d4e1e62c8e6baa24101c8f10029687372fabada
kiarashkia138 <kiarashkia138@gmail.com>
2023-01-08 18:08:15 +0330

    change in read path and working on undo create file is okay and functional

--- projet.c
+++ projet.c
@@ -5,199 +5,141 @@
 #include <conio.h>
 #include <dir.h>
 #include <process.h>
+#include <stdbool.h>
+
+
+# define MAX_line 2048 
 
 
 char string_inpu[256] ;
-char string_path[256] = "D:\\C\\project\\project-kiarashkia138" ;
-char* firstofpath ;
-int end = 1,count_in = 0;
-FILE *fp;
-FILE *undo ;
+char filename[MAX_line] ; // filename 
+int end = 1;
 
-char* concat(const char *s1, const char *s2)
+
+void bringefilename(int start) // will create directory
 {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); 
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+    // char filename[MAX_line] ;
+    // char* filename ;
+    int temp_cnt = 0;
+    int cnt_dir = 0;
+    if(string_inpu[start] == '"')
+    {
+        start += 2; // after slash 
+        // char temp[MAX_line] ;
+        char temp_dir[MAX_line] ;
+        while(string_inpu[start] != '"')
+        {
+            filename[temp_cnt] = string_inpu[start] ;
+            if(string_inpu[start] != '/')
+                temp_dir[temp_cnt] = string_inpu[start] ;
+            else if(string_inpu[start] == '/' && cnt_dir == 0)
+            {
+                cnt_dir++ ;
+                temp_dir[temp_cnt] = string_inpu[start] ;
+            }
+            else if (string_inpu[start] == '/' && cnt_dir > 0)
+            {
+                CreateDirectory (temp_dir, NULL);
+                temp_dir[temp_cnt] = string_inpu[start] ;
+            }
+            start ++ ;
+            temp_cnt ++ ;
+        }
+    }
+
+    else
+    {
+        start++ ; // after slash
+        // char temp[MAX_line] ;
+        char temp_dir[MAX_line] ;
+        while(string_inpu[start] != '\0' && string_inpu[start] != '\n' && string_inpu[start] != ' ')
+        {
+            filename[temp_cnt] = string_inpu[start] ;
+            if(string_inpu[start] != '/')
+                temp_dir[temp_cnt] = string_inpu[start] ;
+            else if(string_inpu[start] == '/' && cnt_dir == 0)
+            {
+                cnt_dir++ ;
+                temp_dir[temp_cnt] = string_inpu[start] ;
+            }
+            else if (string_inpu[start] == '/' && cnt_dir > 0)
+            {
+                CreateDirectory (temp_dir, NULL);
+                temp_dir[temp_cnt] = string_inpu[start] ;
+            }
+            start ++ ;
+            temp_cnt ++ ;
+        }
+    }
+    filename[temp_cnt] = '\0' ;
+}
+
+
+void error(int number) 
+{
+    switch(number)
+    {
+        case 1:
+            printf("coudn't create file \n");
+            break;
+        case 2:
+            printf("coudn't open file \n");
+            break;
+    }
 }
 
 
 void create_func() // start from 16     // will create only one directory need work for second and more directories !!!!!!!!
 {
-    firstofpath = &string_path[0] ;
-    char* dir = &string_path[0] ;
-    int i = 16 ;
-    if(string_inpu[i] == '"') // be careful
-    {
-        i += 2;
-        int siz = 0;
-        while(string_inpu[i] != '"')
-        {
-            char temp[100] ;
-            int j = 0;
-            while(string_inpu[i] != '/' && string_inpu[i] != '"' )
-            {
-                temp[j] = string_inpu[i];
-                j++;
-                i++;
-                siz++ ;
-            }
-            char fin[siz] ;
-            for (int k = 0; k < siz; k++)
-            {
-                fin[k] = temp[k] ;
-            }
-            siz = 0 ;
-            firstofpath = concat(firstofpath, "\\");
-            firstofpath = concat(firstofpath, fin);
-
-            int l = i ;
-            int count_backslashe = 0 ;
-            while(string_inpu[l] != '"')
-            {
-                if(string_inpu[l] == '/')   
-                    count_backslashe++;
-                l++;
-            }
-            if(count_backslashe > 0)
-            {
-                dir = concat(dir,"\\");
-                dir = concat(dir,fin);
-            }
-            if(string_inpu[i] != '"') i++ ;
-        }
-        // need ro check a direcrory exits or not and create it 
-        // char* dirname = "tutorialspoint";
-        // clrscr();
-        // printf("%s\n",dir);
-        CreateDirectory (dir, NULL); // need to check
-        fp = fopen(firstofpath,"a+");  // will creat a file 
-        fclose(fp);
-    }
-    else{
-        // (in this version i am changing to 17)
-        i++;
-        while(string_inpu[i] != '\n' && string_inpu[i] != ' ' && string_inpu[i] != '\0')
-        {
-            char temp[100] ;
-            int siz = 0;
-            int j = 0;
-            while(string_inpu[i] != '/' && string_inpu[i] != '\n' && string_inpu[i] != ' ' && string_inpu[i] != '\0' )
-            {
-                temp[j] = string_inpu[i];
-                j++;
-                i++;
-                siz++;
-            }
-            // add string between 17 & i-1 into "D:\C\project\project-kiarashkia138\"
-            //  add with backslashe !! 
-            char fin[siz] ;
-            for (int k = 0; k < siz; k++)
-            {
-                fin[k] = temp[k] ;
-            }
-            siz = 0 ;
-            firstofpath = concat(firstofpath, "\\");
-            firstofpath = concat(firstofpath, fin);
-            int l = i ;
-            int count_backslashe = 0 ;
-            while(string_inpu[l] != '\n' && string_inpu[l] != ' ' && string_inpu[l] != '\0')
-            {
-                if(string_inpu[l] == '/')   
-                    count_backslashe++;
-                l++;
-            }
-            if(count_backslashe > 0)
-            {
-                dir = concat(dir,"\\");
-                dir = concat(dir,fin);
-            }
-            i++ ;
-        }
-        // need ro check a direcrory exits or not and create it
-        // printf("%s\n",dir);
-        // printf("%s\n",firstofpath);
-        CreateDirectory (dir, NULL); 
-        fp = fopen(firstofpath,"a+");  // will creat a file 
-        fclose(fp);
-    }
+    bringefilename(16);
+    FILE* file = fopen(filename,"a");
+    if(file == NULL)
+        error(1) ;
+    fclose(file);
 }
 
-void insert_func() // start from 15
+
+
+// undo 
+void undo_func() // will copy undo temp to this file
 {
-    firstofpath = &string_path[0] ;
-    int i = 15 ;
-    if(string_inpu[i] == '"') // be careful
-    {
-        i += 2;
-        int siz = 0;
-        while(string_inpu[i] != '"')
-        {
-            char temp[100] ;
-            int j = 0;
-            while(string_inpu[i] != '/' && string_inpu[i] != '"' )
-            {
-                temp[j] = string_inpu[i];
-                j++;
-                i++;
-                siz++ ;
-            }
-            char fin[siz] ;
-            for (int k = 0; k < siz; k++)
-            {
-                fin[k] = temp[k] ;
-            }
-            siz = 0 ;
-            firstofpath = concat(firstofpath, "\\");
-            firstofpath = concat(firstofpath, fin);
-            if(string_inpu[i] != '"') i++ ;
-        }
-        fp = fopen(firstofpath,"a+");  // will creat a file 
-        //  i am in file 
-
-        fclose(fp);
-    }
-    else{
-        while(string_inpu[i] != '\n' && string_inpu[i] != ' ' && string_inpu[i] != '\0')
-        {
-            char temp[100] ;
-            int siz = 0;
-            int j = 0;
-            while(string_inpu[i] != '/' && string_inpu[i] != '\n' && string_inpu[i] != ' ' && string_inpu[i] != '\0' )
-            {
-                temp[j] = string_inpu[i];
-                j++;
-                i++;
-                siz++;
-            }
-            // add string between 17 & i-1 into "D:\C\project\project-kiarashkia138\"
-            //  add with backslashe !! 
-            char fin[siz] ;
-            for (int k = 0; k < siz; k++)
-            {
-                fin[k] = temp[k] ;
-            }
-            siz = 0 ;
-            firstofpath = concat(firstofpath, "\\");
-            firstofpath = concat(firstofpath, fin);
-            i++ ;
-        }
-        fp = fopen(firstofpath,"a+");  // will creat a file 
-        fclose(fp);
-    }
+    
 }
 
+void save_for_undo() // must be used before undo
+{
+    char temp[MAX_line] = "root/undo/temp";
+    int i = 0 , j = 0;
+    char format[10] ;
+    while(filename[i] != '\0' && filename[i] != '\n') // for file format
+    {
+        if(filename[i] == '.')
+        {
+            format[j] = filename[i];
+            j++ ;
+        }
+        i++ ;    
+    }
+    strcat(temp,format) ;
+    FILE* undo = fopen(temp,"w") ;
+    FILE* file = fopen(filename,"r") ;
+
+    char buffer[MAX_line] ;
+    bool keep_reading = true ;
+    while(keep_reading)
+    {
+        fgets(buffer,MAX_line,file) ;
+        if(feof(file)) keep_reading = false ;
+        else fputs(buffer,undo) ;
+    }
+}
 
 void check()
 {
     int eenndd = strstr(string_inpu,"exit");
     int create = strstr(string_inpu,"createfile--file");
     int insert = strstr(string_inpu,"insertstr--file");
-    // int temp_wi = strstr(string_str,withdraw_string);
-    // int temp_ba = strstr(string_str,balance_string);
-    // int temp_tr = strstr(string_str,transfer_string);
-    // int temp_in = strstr(string_str,info_string);
+    int undoo = strstr(string_inpu,"undo--file");
     
     if(eenndd)
         end = 0 ;
@@ -205,18 +147,25 @@ void check()
     {
         create_func();
     }
-    else if(insert)
+    // else if(insert)
+    // {
+    //     // insert_func();
+    // }
+    else if(undoo)
     {
-        insert_func();
+        undo_func();
     }
     else
         printf("Invalid input\n");
 }
+
+
 void input_in()
 {
     fgets(string_inpu,256,stdin);
     check();
 }
+
 
 int main()
 {
